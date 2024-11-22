2bb36204256dff947bbc3713240b0a860ff6c5c7
kornel <kornel@mail.com>
2024-11-22 09:02:56 +0100

    dodanie zadania 2

---
+++ Zadanie2Kalkulator/kalkulator.js
@@ -0,0 +1,23 @@
+class Calculator{
+    add(a, b){
+        return a + b
+    }
+
+    substruct(a, b){
+        return a - b
+    }
+
+    multiply(a, b){
+        return a * b
+    }
+
+    divide(a, b){
+        return a / b
+    }
+}
+
+console.log("Podaj liczbę a: ")
+const a = 30
+const b = 30
+
+console.log()
