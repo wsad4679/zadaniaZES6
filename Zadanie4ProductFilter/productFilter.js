835b326b0ccdb9cd5dccd6b5a2ea56df2c858147
SzymonH <szymonh4679@gmail.com>
2024-11-28 20:07:02 +0100

    Begin product filter app

---
+++ Zadanie4ProductFilter/productFilter.js
@@ -0,0 +1,9 @@
+const products = [
+    { name: 'Orange', price: 1.25, quantity: 2, category: 'Fruits' },
+    { name: 'Milk', price: 0.99, quantity: 1, category: 'Dairy' },
+    { name: 'Bread', price: 1.50, quantity: 1, category: 'Bakery' },
+    { name: 'Chicken Breast', price: 5.99, quantity: 1, category: 'Meat' },
+    { name: 'Broccoli', price: 2.00, quantity: 3, category: 'Vegetables' },
+    { name: 'Rice', price: 1.20, quantity: 1, category: 'Grains' },
+    { name: 'Apple Juice', price: 2.50, quantity: 2, category: 'Beverages' }
+  ];
