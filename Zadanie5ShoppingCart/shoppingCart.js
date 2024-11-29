d7b37662724da0056ae6b87277250c3ad92e22ff
szymonH <szymonH4679@gmail.com>
2024-11-29 08:13:39 +0100

    Finished shopping cart application

--- Zadanie5ShoppingCart/shoppingCart.js
+++ Zadanie5ShoppingCart/shoppingCart.js
@@ -17,6 +17,20 @@ class Cart{
         this.list.push(product)
     }
 
+    removeProduct(productName){
+        let i = 0
+        let d = 0
+        this.list.forEach(product => {
+            i++
+            if (product.name == productName)
+                {
+                    i = d
+                }
+        });
+
+        this.list.splice(d,1)
+    }
+
 
 }
 
@@ -25,5 +39,6 @@ const lista = new Cart()
 lista.addProduct({name: 'Jabko', price: 12, quantity: 1})
 lista.addProduct({name: 'Jabko', price: 12, quantity: 1})
 lista.addProduct({name: 'Jabko', price: 12, quantity: 1})
+lista.removeProduct("Jabko")
 lista.showCart()
 
